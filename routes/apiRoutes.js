7bbdd28d1248009cdebdd833501d3a38ed6b3e37
walidhkhan <walidhkhan@gmail.com>
2021-08-29 21:52:04 -0700

    make it better

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -7,13 +7,27 @@ router.get('/notes', (req, res) => {
 });
 
 router.post('/notes', (req, res) => {
-
-    const {title, text} = req.body;
-    const newNote = {title,text};
+    req.body.id = db.length.toString();
+    const { id, title, text } = req.body;
+    const newNote = { id, title, text };
     db.push(newNote);
     fs.writeFileSync('db/db.json', JSON.stringify(db));
-    res.JSON(db);
+    res.json(db);
 })
 
-module.exports = router;
+
+router.delete('/notes/:id', (req, res) => {
+    db.splice(db.findIndex(v => v.id === req.params.id), 1);
+    // const result = db.filter(note => note.id === req.params.id)[0];
+    // if (result) {
+    //     db.splice(result);
+    // } else {
+    //     res.send(404);
+    // };
+
+    fs.writeFileSync('db/db.json', JSON.stringify(db));
+        res.json(db);
+    });
+
+    module.exports = router;
 
